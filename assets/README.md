dc57e58cb7519bf63a685f7ba11689dbec8c1131
Perluceo <FullStackDevSMU@gmail.com>
2019-06-13 12:10:42 -0500

    redo of git repository

---
+++ assets/README.md
@@ -0,0 +1,3 @@
+# unit-4-game
+
+The famous game of Black Jack or 21 is the underlying premise of Crystal Collector. Instead of counting a deck or decks of 52 cards you must keep track of the value each crystal possesses to equate the target number. If you exceed the target you lose if you meet it you win.  Good luck!!
---
+++ assets/javascript/game.js
@@ -0,0 +1,79 @@
+// crystal variables
+var blue = 0;
+var green = 0;
+var purple = 0;
+var red = 0;
+
+// target and your score variables
+var targetScore = 0;
+var currentScore = 0;
+
+// wins and losses variables
+var winCount = 0;
+var lossCount = 0;
+
+
+// functions
+
+// allows 5 random numbers crystals and target
+var randomNumber = function(min, max) {
+    console.log(min,max);
+    return Math.floor(Math.random() * (max -min + 1)) + min;
+}
+
+var startGame = function() {
+    console.log("START")
+    //reset current score 
+    var yourNumber = 0;
+
+    //set target score range
+    targetScore = randomNumber(25, 183);
+
+    //set crystal values
+    blue     = randomNumber (1, 12);
+    green    = randomNumber (1, 12);
+    purple   = randomNumber (1, 12);
+    red      = randomNumber (1, 12);
+    console.log(blue)
+
+    //inner.html
+    $("#yourScore").html(yourScore);
+    $("#targetScore").html(targetScore);
+
+ 
+    console.log ("-------------------------");
+    console.log ("Target Score: " + targetScore);
+    console.log("blue: " + blue + "|green: " + green + " | purple: " + purple + "| red:" +
+    red);
+    console.log ("-------------------------");
+
+    // responds to crystals
+    //var yourScore = getElementById(red, blue, yellow, green.onclick(math.sum));
+    yourScore.innerHTML = 0;
+    targetScore.innerHTML = getRandomValue(10, 4);
+
+    function addToScore(val) {
+    var numberToAdd = parseInt(val);
+    var currentScore = parseInt(yourScore.innerHTML);
+    yourScore.innerHTML = numberToAdd + currentScore;
+};
+};
+
+$("#blue").click(function() {
+    console.log(blue);
+});
+
+$("#green").click(function() {
+    console.log(green);
+});
+
+$("#purple").click(function() {
+    console.log(purple);
+});
+
+$("#red").click(function() {
+    console.log(red);
+});
+
+startGame();
+
